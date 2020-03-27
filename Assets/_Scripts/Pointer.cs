de6aa289280c750b9a30bd3c7694ebfe4f28a1cd
[sayed-salah] <[sayed.salah2015@outlook.com]>
2020-03-27 17:11:48 +0200

    First commit

---
+++ Assets/_Scripts/Pointer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public GameObject player;
+    public LayerMask wallsfloorlayer;
+    private RaycastHit hit;
+    private EventSystem eventSystem;
+
+   
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        eventSystem = EventSystem.current;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit,100, wallsfloorlayer))
+        {
+            if (hit.collider.tag == "Floor")
+            {
+                if ( Input.GetMouseButtonDown(0))
+                {
+                    Vector3 position = player.transform.position;
+
+                    Vector3 newposition = hit.point;
+
+                    position.x = newposition.x;
+                    position.z = newposition.z;
+                    player.transform.position = position;
+                }
+            }
+
+        }
+        
+    }
+}
